7e3a0c8d78e9c0e2a044790bdb601810780e761d
Prof Edir <ecaramba@gmail.com>
2025-07-03 21:46:09 -0300

    exercios

--- exercicios.js
+++ exercicios.js
@@ -25,3 +25,51 @@ function ex61()
     resposta.innerText = conta + " 1 = " + total;
     
 }
+
+// finonacci
+function ex60()
+{
+    var valor = texto.value;
+
+    var n1 = 1;
+    var n2 = 1;
+
+    var ini = 3;
+    
+    var prox = 1;
+
+    resposta.innerText = "1,1,";
+
+    while (ini <= valor)
+    {
+        prox = n1 + n2;
+        n1 = n2;
+        n2 = prox;
+        ini += 1;
+
+        resposta.innerText += prox + ","
+    }
+}
+
+function ex59()
+{
+    
+    var quantPar   = 0;
+    var quantImpar = 0;
+
+    for (var ini = 1; ini <= 10; ini++)
+    {
+        var num = prompt("digite um numero");
+        var div = num % 2;
+
+        if (div == 0)
+        {
+            quantPar += 1;
+        } else {
+            quantImpar += 1;
+        }
+
+    }
+
+    resposta.innerText = "Num Pares: "+quantPar + "Num Impar: " + quantImpar;
+}
