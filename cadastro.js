f38fe13a19e79048a6048bf73d9a89b5c7ca6add
Prof Edir <ecaramba@gmail.com>
2025-07-08 20:22:50 -0300

    envio de form

--- cadastro.js
+++ cadastro.js
@@ -9,27 +9,39 @@ $(document).ready(function(){
         var telefone = $('#telefone').val();
         var email = $('#email').val();
         var cidade = $('#cidade').val();
+        var erro = false;
 
         $('input, select').removeClass('is-invalid');
 
         if (nome.trim() == '')
         {
             $('#nome').addClass('is-invalid');
+            erro = true;
         }
 
         if (telefone.trim() == '')
         {
             $('#telefone').addClass('is-invalid');
+            erro = true;
         }
 
         if (email.trim() == '')
         {
             $('#email').addClass('is-invalid');
+            erro = true;
         }
 
         if (cidade == '')
         {
             $('#cidade').addClass('is-invalid');
+            erro = true;
+        }
+
+        if (erro == true)
+        {
+            alert("Existem erros no formulario")
+        } else {
+            $.post("https://www.google.com")
         }
 
 
